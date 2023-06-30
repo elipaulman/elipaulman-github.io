a46dd493a2eeb4bcaf8453c82b114b65c78b7680
Elijah Paulman <eli.paulman@gmail.com>
2023-06-30 15:51:57 -0400

    weather fix

--- weather.js
+++ weather.js
@@ -6,6 +6,7 @@ let summary = document.querySelector(".summary");
 let loc = document.querySelector(".location");
 let icon = document.querySelector(".icon");
 const kelvin = 273;
+const api = "a492a768764e969820914c25cd2b788a";
 
 window.addEventListener("load", () => {
   if (navigator.geolocation) {
@@ -14,11 +15,8 @@ window.addEventListener("load", () => {
       lon = position.coords.longitude;
       lat = position.coords.latitude;
 
-      // API ID
-      const api = "a492a768764e969820914c25cd2b788a";
-
       // API URL
-      const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=a492a768764e969820914c25cd2b788a`;
+      const base = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${api}`;
 
       // Calling the API
       fetch(base)
@@ -27,12 +25,13 @@ window.addEventListener("load", () => {
         })
         .then((data) => {
           console.log(data);
-          const temperatureInCelsius = Math.floor(data.main.temp - kelvin);
+          const temperatureInKelvin = data.current.temp;
+          const temperatureInCelsius = Math.floor(temperatureInKelvin - kelvin);
           const temperatureInFahrenheit = Math.floor((temperatureInCelsius * 9) / 5 + 32);
           temperature.textContent = temperatureInFahrenheit + "°F";
-          summary.textContent = data.weather[0].description;
-          loc.textContent = data.name + "," + data.sys.country;
-          let icon1 = data.weather[0].icon;
+          summary.textContent = data.current.weather[0].description;
+          loc.textContent = data.timezone;
+          let icon1 = data.current.weather[0].icon;
           icon.innerHTML = `<img src="icons/${icon1}.svg" style='height:10rem'/>`;
         });
     });
