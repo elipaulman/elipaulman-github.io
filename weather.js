3a9fc1956dff975710fc555f5a028032d58d8360
Elijah Paulman <eli.paulman@gmail.com>
2023-07-01 13:56:04 -0400

    weather update 7123

--- weather.js
+++ weather.js
@@ -1,7 +1,7 @@
 // Declaring the variables
 let lon;
 let lat;
-let currentWeatherContainer = document.querySelector(".container");
+let currentWeatherContainer = document.querySelector(".current-conditions");
 let forecastList = document.querySelector(".forecast-list");
 const kelvin = 273;
 const apiKey = "a492a768764e969820914c25cd2b788a";
@@ -9,116 +9,152 @@ const apiKey = "a492a768764e969820914c25cd2b788a";
 window.addEventListener("load", () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position);
       lon = position.coords.longitude;
       lat = position.coords.latitude;
-
-      // API URLs
-      const currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-      const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-
-      // Clear existing forecast entries
-      forecastList.innerHTML = '';
-
-      // Fetching current weather data
-      fetch(currentWeatherURL)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Current weather data not available");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          console.log(data);
-          const temperatureInCelsius = Math.floor(data.main.temp - kelvin);
-          const temperatureInFahrenheit = Math.floor((temperatureInCelsius * 9) / 5 + 32);
-
-          // Update current weather container
-          currentWeatherContainer.innerHTML = `
-            <div class="icon">
-              <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" style="height: 5rem;" />
-            </div>
-            <div class="temp">${temperatureInFahrenheit}°F</div>
-            <div class="summary">${data.weather[0].description}</div>
-            <div class="rain-chance">Rain: ${data.main.humidity}%</div>
-            <div class="location">${data.name}</div>
-          `;
-
-          // Update page background based on weather condition
-          updatePageBackground(data.weather[0].main);
-        })
-        .catch((error) => {
-          console.log("Error fetching current weather data:", error);
-        });
-
-      // Fetching forecast data
-      fetch(forecastURL)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Forecast data not available");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          console.log(data);
-          const forecasts = data.list;
-
-          // Group the forecasts by date
-          const groupedForecasts = {};
-          forecasts.forEach((forecast) => {
-            const forecastDate = new Date(forecast.dt_txt.split(' ')[0]).toDateString();
-            if (!groupedForecasts[forecastDate]) {
-              groupedForecasts[forecastDate] = [];
-            }
-            groupedForecasts[forecastDate].push(forecast);
-          });
-
-          // Iterate through the grouped forecasts
-          Object.entries(groupedForecasts).forEach(([date, forecasts]) => {
-            const temperatureHighArray = [];
-            const temperatureLowArray = [];
-
-            forecasts.forEach((forecast) => {
-              const temperatureInCelsius = Math.floor(forecast.main.temp - kelvin);
-              const temperatureInFahrenheit = Math.floor((temperatureInCelsius * 9) / 5 + 32);
-              temperatureHighArray.push(temperatureInFahrenheit);
-              temperatureLowArray.push(temperatureInFahrenheit);
-            });
-
-            const temperatureHighInFahrenheit = Math.max(...temperatureHighArray);
-            const temperatureLowInFahrenheit = Math.min(...temperatureLowArray);
-
-            const forecastItem = document.createElement("div");
-            forecastItem.classList.add("forecast-item");
-            forecastItem.innerHTML = `
-              <div class="forecast-date">${date}</div>
-              <div class="forecast-temp">High: ${temperatureHighInFahrenheit}°F | Low: ${temperatureLowInFahrenheit}°F</div>
-              <div class="forecast-icon">
-                <img src="https://openweathermap.org/img/wn/${forecasts[0].weather[0].icon}.png" style="height: 8rem;" />
-              </div>
-            `;
-            forecastList.appendChild(forecastItem);
-          });
-        })
-        .catch((error) => {
-          console.log("Error fetching forecast data:", error);
-        });
+      fetchWeatherData();
     });
   } else {
     console.log("Geolocation is not supported by this browser.");
   }
 });
 
-// Update the page background based on the weather condition
+function fetchWeatherData() {
+  // API URLs
+  const currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+  // Clear existing forecast entries
+  forecastList.innerHTML = '';
+
+  // Fetching current weather data
+  fetch(currentWeatherURL)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Current weather data not available");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      updateCurrentWeather(data);
+    })
+    .catch((error) => {
+      console.log("Error fetching current weather data:", error);
+    });
+
+  // Fetching forecast data
+  fetch(forecastURL)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Forecast data not available");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      updateForecast(data);
+    })
+    .catch((error) => {
+      console.log("Error fetching forecast data:", error);
+    });
+}
+
+function updateCurrentWeather(data) {
+  const temperatureInCelsius = Math.floor(data.main.temp - kelvin);
+  const temperatureInFahrenheit = Math.floor((temperatureInCelsius * 9) / 5 + 32);
+
+  // Update current weather container
+  currentWeatherContainer.innerHTML = `
+    <div class="icon">
+      <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" style="height: 5rem;" />
+    </div>
+    <div class="temp">${temperatureInFahrenheit}°F</div>
+    <div class="summary">${data.weather[0].description}</div>
+    <div class="rain-chance">Rain: ${data.main.humidity}%</div>
+    <div class="location">${data.name}</div>
+  `;
+
+  // Update page background based on weather condition
+  updatePageBackground(data.weather[0].main);
+}
+
+function updateForecast(data) {
+  const forecasts = data.list;
+
+  // Group the forecasts by date
+  const groupedForecasts = {};
+  forecasts.forEach((forecast) => {
+    const forecastDate = new Date(forecast.dt_txt.split(' ')[0]).toDateString();
+    if (!groupedForecasts[forecastDate]) {
+      groupedForecasts[forecastDate] = [];
+    }
+    groupedForecasts[forecastDate].push(forecast);
+  });
+
+  // Iterate through the grouped forecasts
+  Object.entries(groupedForecasts).forEach(([date, forecasts]) => {
+    const temperatureHighArray = forecasts.map((f) => Math.floor((f.main.temp_max - kelvin) * 9 / 5 + 32));
+    const temperatureLowArray = forecasts.map((f) => Math.floor((f.main.temp_min - kelvin) * 9 / 5 + 32));
+    const temperatureHigh = Math.max(...temperatureHighArray);
+    const temperatureLow = Math.min(...temperatureLowArray);
+    const iconCode = forecasts[0].weather[0].icon;
+
+    // Create forecast item
+    const forecastItem = document.createElement("div");
+    forecastItem.classList.add("forecast-item");
+    forecastItem.innerHTML = `
+      <div class="forecast-date">${date}</div>
+      <div class="forecast-temp">${temperatureLow}°F - ${temperatureHigh}°F</div>
+      <div class="forecast-icon"><img src="https://openweathermap.org/img/wn/${iconCode}.png" alt="Weather Icon" /></div>
+    `;
+
+    // Append forecast item to forecast list
+    forecastList.appendChild(forecastItem);
+  });
+}
+
+
 function updatePageBackground(weatherCondition) {
+  // Update background color or image based on weather condition
+  // Example implementation, you can customize this based on your preferences
   const body = document.querySelector("body");
-  if (weatherCondition.includes("Cloud")) {
-    body.style.background = "linear-gradient(rgb(104, 159, 184), rgb(37, 94, 163))";
-  } else if (weatherCondition.includes("Rain")) {
-    body.style.background = "linear-gradient(rgb(20, 47, 81), rgb(6, 15, 26))";
-  } else if (weatherCondition.includes("Clear")) {
-    body.style.background = "linear-gradient(rgb(25, 117, 210), rgb(9, 47, 85))";
-  } else {
-    body.style.background = "linear-gradient(rgb(3, 35, 63), rgb(0, 0, 0))";
+  switch (weatherCondition) {
+    case "Clear":
+      body.style.backgroundColor = "#5dbcd2";
+      break;
+    case "Clouds":
+      body.style.backgroundColor = "#607d8b";
+      break;
+    case "Rain":
+      body.style.backgroundImage = "url('rainy-background.jpg')";
+      body.style.backgroundSize = "cover";
+      break;
+    default:
+      body.style.backgroundColor = "#4caf50";
   }
 }
+
+// Event listener for the "Get Weather" button
+const getWeatherButton = document.getElementById("get-weather-button");
+getWeatherButton.addEventListener("click", () => {
+  const locationInput = document.getElementById("location-input");
+  const location = locationInput.value.trim();
+
+  if (location) {
+    const geocodingAPI = `https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${apiKey}`;
+
+    fetch(geocodingAPI)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Location not found");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        lat = data[0].lat;
+        lon = data[0].lon;
+        fetchWeatherData();
+      })
+      .catch((error) => {
+        console.log("Error fetching geolocation data:", error);
+      });
+  }
+});
