a680439c0561961c38ba153a887dfbdd63238cf9
elipaulman <eli.paulman@gmail.com>
2025-03-12 23:13:29 -0400

    added skills section

--- js/custom.js
+++ js/custom.js
@@ -1,97 +1,116 @@
-
-  $(function () {
-    'use strict'
-
+$(function () {
+    'use strict';
+  
     // MENU
-    $('.navbar .nav-link').on('click',function(){
-        $(".navbar-collapse").collapse('hide');
+    $('.navbar .nav-link').on('click', function () {
+      $(".navbar-collapse").collapse('hide');
     });
-
-    $(window).on('scroll', function() {     
-                                
-        /*----------------------------------------------------*/
-        /*  Navigtion Menu Scroll
-        /*----------------------------------------------------*/    
-        
-        var b = $(window).scrollTop();
-        
-        if( b > 72 ){       
-            $(".navbar").addClass("scroll");
-        } else {
-            $(".navbar").removeClass("scroll");
-        }               
+  
+    $(window).on('scroll', function () {
+  
+      /*----------------------------------------------------*/
+      /*  Navigation Menu Scroll
+      /*----------------------------------------------------*/
+  
+      var b = $(window).scrollTop();
+  
+      if (b > 72) {
+        $(".navbar").addClass("scroll");
+      } else {
+        $(".navbar").removeClass("scroll");
+      }
     });
-
+  
     // TESTIMONIALS CAROUSEL
     $('#testimonials-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-            },
-            900:{
-                items:2,
-            },
-            1200:{
-                items:3,
-                loop:false
-            }
+      loop: true,
+      margin: 10,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        900: {
+          items: 2,
+        },
+        1200: {
+          items: 3,
+          loop: false
         }
-    })
-
+      }
+    });
+  
     // EXPERIENCE CAROUSEL
     $('#experience-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        dots: true,
-        autoplay: false,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-            },
-            900:{
-                items:2,
-            },
-            1200:{
-                items:3,
-                loop:false
-            }
+      loop: true,
+      margin: 10,
+      dots: true,
+      autoplay: false,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        900: {
+          items: 2,
+        },
+        1200: {
+          items: 3,
+          loop: false
         }
-    })
-
+      }
+    });
+  
     // PROJECTS CAROUSEL
     $('#projects-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        autoplay: false,
-        dots: true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-            },
-            900:{
-                items:2,
-            },
-            1200:{
-                items:3,
-                loop:false
-            }
+      loop: true,
+      margin: 10,
+      autoplay: false,
+      dots: true,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        900: {
+          items: 2,
+        },
+        1200: {
+          items: 3,
+          loop: false
         }
-    })
-
+      }
+    });
+  
     // SMOOTHSCROLL
-    $(function() {
-      $('.navbar .nav-link').on('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 49
-        }, 1000);
-        event.preventDefault();
-      });
-    });   
-     
+    $('.navbar .nav-link').on('click', function (event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 49
+      }, 1000);
+      event.preventDefault();
+    });
+  
+    // Skills Section Animation
+    function skillsAnimation() {
+      var skillsSection = $('#skills'); // Updated to select by ID
+      if (skillsSection.length) {
+        var skillsOffset = skillsSection.offset().top - $(window).height() + 100; // Trigger a bit before
+  
+        if ($(window).scrollTop() > skillsOffset) {
+          $('.skill-item').each(function (i) {
+            var $this = $(this);
+            setTimeout(function () {
+              $this.addClass('animate');
+            }, 150 * (i + 1)); // Stagger the animations
+          });
+        }
+      }
+    }
+  
+    // Initial check on document ready
+    skillsAnimation();
+  
+    // Scroll event listener
+    $(window).on('scroll', skillsAnimation);
   });
